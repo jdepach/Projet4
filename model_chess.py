b78592b618045f02ec2fe3436d65bbdab525e539
trashpanda <juliedepach@gmail.com>
2021-10-12 19:49:54 +0200

    lecture csv

--- model_chess.py
+++ model_chess.py
@@ -17,12 +17,28 @@
         # quick_chess : 30 min
     # description
 
-# class player
-    # family_name
-    # name
-    # birthday
-    # sexe
-    # ranking (int positive)
+class player:
+    def __init__(self, family_name, name, birthday, gender, ranking):
+        self.name = name
+        self.family_name = family_name
+        self.birthday = birthday
+        self.gender = gender
+        self.ranking = ranking
+        self.id = id(self) # code de chaque joueur
+
+    def set_name(self, input_name):
+        name = input_name
+    def set_family_name(input_family_name):
+        family_name = input_family_name
+    def set_birthday(input_birthday):
+        birthday = input_birthday
+    def set_gender(input_gender):
+        gender = input_gender
+    def set_ranking(input_ranking):
+        ranking = input_ranking
+
 
 # round = [match1, match2; match3, match4]
 # match = ([player1 ; score_p1], [player2 ; score_p2])
+
+
--- view_chess.py
+++ view_chess.py
@@ -1,3 +1,44 @@
+from os.path import isfile
+
+import model_chess as mc
+from os.path import isfile
+import csv
+
+menu = "MENU PRINCIPAL \n Pour ajouter des joueurs tapez 1\n Pour lancer un tournois tapez 2\n"
+answer = input(menu)
+
+if answer == "3" and isfile("players.csv"):
+
+    with open("players.csv", "r") as f:
+        reader = csv.reader(f)
+        for row in reader:
+            players = [row[0], row[1], row[2], row[3], row[4]]
+            print(players)
+    mc.player.set_family_name = players[0]
+    mc.player.set_name = players[1]
+    mc.player.set_birthday = players[2]
+    mc.player.set_gender = players[3]
+    mc.player.set_ranking = players[4]
+    player = mc.player()
+    output = 'family name:%d, name:%s, birthday:%s, gender:%s, ranking:%d' % (
+    player.family_name, player.name, player.birthday, player.gender, player.ranking)
+    print(output)
+
+    # temp = dir(player)
+    # for item in temp:
+    #     print(item, ' : ', temp[item])
+
+else:
+    if answer == "1":
+        a = input("prenom : ")
+        mc.player.input_name = a
+        player = mc.player()
+        print(player.name)
+
+
+    elif answer == "2":
+        print("let's go")
+
 
 # menu
     # add_players
